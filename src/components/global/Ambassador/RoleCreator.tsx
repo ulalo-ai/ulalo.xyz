04e260edf39b33d1e8110ae0405c13638db3b629
mehak-2 <Kangmehak167@gmail.com>
2025-04-05 14:57:17 +0530

    changes

--- src/components/global/Ambassador/RoleCreator.tsx
+++ src/components/global/Ambassador/RoleCreator.tsx
@@ -3,29 +3,39 @@ import RightClickCard from './RightClickCard'
 
 const datas = [
     {
-        title:"Your Role",
+        title:"Your Role as a ULALO Ambassador",
+        description:"As an ambassador, you will play a dynamic role in growing and supporting the ULALO ecosystem. Your contributions may include:",
         points:[
-            "Promote and engage with the ULALO community and brand.",
-            "Increase ULALO ‘s awareness and image."
+            "Promoting the ULALO brand across social media and Web3 platforms",
+            "Creating original and engaging content (threads, videos, graphics, etc.)",
+            "Participating in community discussions and answering questions",
+            "Hosting or joining events, AMAs, and campaigns",
+            "Supporting regional growth and localized awareness",
+            "Identifying partnership or collaboration opportunities",
         ]
     },
-    {
-        title:"Content Creator",
-        points:[
-            "Create original content tailored for various platforms, including text posts, video, and promotional material.",
-        ]
-    },
+    // {
+    //     title:"Content Creator",
+    //     points:[
+    //         "Create original content tailored for various platforms, including text posts, video, and promotional material.",
+    //     ]
+    // },
 ]
 
 const RoleCreator = () => {
   return (
-    <div className="w-full grid grid-cols-1 md:grid-cols-2 gap-8 justify-center self-stretch">
+    <div className="w-full grid  gap-8 justify-center self-stretch">
         {datas.map((data, index) => (
             <div key={index} className="flex flex-col items-start gap-4 p-4">
                 <p className="text-[#1BE866] font-sfpro_700 text-2xl leading-[160%] tracking-[-0.48px] uppercase">
                     {data.title}
                 </p>
+                <p className="text-[#364655] font-openSans_Regular text-[15px] leading-[160%]">
+                    {data.description}
+                </p>
                 <RightClickCard data={data.points} />
+                <p className="text-[#364655] font-openSans_Regular text-[15px] leading-[160%]">Whether you're a content creator, community builder, educator, or strategist—there’s a valuable role for you in the ULALO movement.
+                </p>
             </div>
         ))}
     </div>
